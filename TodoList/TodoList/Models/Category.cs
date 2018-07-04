8dd1364f6b2bc309aee814d7384d973302e569ef
Nabil <abilaz24@gmail.com>
2018-07-04 17:15:47 +0200

    Ajout des migrations

---
+++ TodoList/TodoList/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TodoList.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage ="Le champ nom est obligatoire")]
+        [RegularExpression("^[A-Z][a-z0-9_-]{5, 20}$", ErrorMessage = "Mauvais format")]
+
+        public string Name { get; set; }
+
+        //public ICollection<Todo> Todos { get; set; }
+    }
+}
